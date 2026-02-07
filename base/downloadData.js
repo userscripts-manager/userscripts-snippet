14d088386eaa18d74ec2a60d1d8b9dc068c4c670
gissehel <public-dev-git-capella@gissehel.org>
2026-02-07 16:24:19 +0100

    FIX : Fix downloadData when called without mimetype

--- base/downloadData.js
+++ base/downloadData.js
@@ -1,10 +1,9 @@
 // @import{createElementExtended}
-
 /**
  * Download data as a file
  * 
  * @param {string} filename - The name of the file
- * @param {string} data - The data to download
+ * @param {string|Uint8Array} data - The data to download
  * @param {object} options - The options
  * @param {string} options.mimetype - The mimetype of the data
  * @param {string} options.encoding - The encoding to use on the text data if provided
@@ -13,7 +12,7 @@ const downloadData = (filename, data, options) => {
     if (!options) {
         options = {}
     }
-    const { mimetype, encoding } = options
+    let { mimetype, encoding } = options
     if (!mimetype) {
         mimetype = 'application/octet-stream'
     }
