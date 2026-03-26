5d85b1035e51608ba19587fbcf4b22f67d104fa6
gissehel <public-dev-git-capella@gissehel.org>
2026-03-26 21:33:19 +0100

    REFACT : Cleanup and change API

--- base/Semaphore.js
+++ base/Semaphore.js
@@ -7,7 +7,8 @@ class Semaphore {
      * Constructor for Semaphore.
      * @param {Number} maxConcurrent The maximum number of concurrent operations allowed.
      */
-    constructor(maxConcurrent = 1) {
+    constructor(name, maxConcurrent = 1) {
+        this.name = name;
         this.maxConcurrent = maxConcurrent;
         this.current = 0;
         this.queue = [];
@@ -21,19 +22,13 @@ class Semaphore {
      * @returns A promise that resolves when the semaphore is acquired.
      */
     async acquire(name = '') {
-        if (this.debug) {
-            console.log(`acquire - ${name} (${this.queue.length})`)
-        }
         if (this.current < this.maxConcurrent) {
             this.current++;
             return Promise.resolve();
         }
 
-        return new Promise(resolve => {
+        return new Promise((resolve) => {
             this.queue.push(() => {
-                if (this.debug) {
-                    console.log(`unlocked - ${name} (${this.queue.length})`)
-                }
                 resolve();
             });
         });
@@ -44,10 +39,7 @@ class Semaphore {
      * 
      * @param {String} name The name or identifier for the release request. (for logging/debugging purposes)
      */
-    release(name = '') {
-        if (this.debug) {
-            console.log(`release - ${name} (${this.queue.length})`)
-        }
+    async release(name = '') {
         if (this.queue.length > 0) {
           const next = this.queue.shift();
           next();
