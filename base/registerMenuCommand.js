180ab7b79e68d795d37331da962a66b947df166f
gissehel <public-dev-git-capella@gissehel.org>
2026-05-08 11:51:03 +0200

    Use register pattern on registerMenuCommand

--- base/registerMenuCommand.js
+++ base/registerMenuCommand.js
@@ -1,9 +1,19 @@
 // @grant{GM.registerMenuCommand}
+// @grant{GM.unregisterMenuCommand}
 /**
- * Register a menu command in the userscript manager's menu (e.g., Tampermonkey, Greasemonkey, Violentmonkey).
+ * Register a menu command in the userscript manager's menu (e.g., Tampermonkey, Greasemonkey, Violentmonkey). Unlike the underlying `GM.registerMenuCommand`, this function use the register pattern, thus returns an unregister function that can be called to remove the menu command when it's no longer needed.
+ * 
  * @param {string} name The name of the menu command to display.
  * @param {() => void} callback The function to execute when the menu command is selected.
+ * @return {() => Promise<void>} A function that, when called, will unregister the menu command.
  */
-const registerMenuCommand = (name, callback) => {
-    GM.registerMenuCommand(name, callback);
+const registerMenuCommand = async (name, callback) => {
+    let id = await GM.registerMenuCommand(name, callback);
+    return async () => {
+        if (id !== null) {
+            const tempId = id;
+            id = null;
+            await GM.unregisterMenuCommand(tempId);
+        }
+    }
 }
