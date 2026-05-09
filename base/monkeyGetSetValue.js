abb36b8490499f478d03288cc4d1fe8447e9fc36
gissehel <public-dev-git-capella@gissehel.org>
2026-05-09 20:16:08 +0200

    Fix jsdoc for monkeyGetSetValue : Set value type as template

--- base/monkeyGetSetValue.js
+++ base/monkeyGetSetValue.js
@@ -3,9 +3,10 @@
 /**
  * Get the value from the monkey (Tampermonkey/Greasemonkey/Violentmonkey/etc.) storage, and set them with the default if nothing already exists
  * 
+ * @template T
  * @param {String} key The key to use to name the value to get or set
- * @param {Object} value The default value to set and return if not defined
- * @returns {Promise<Object>} The value to use
+ * @param {T} value The default value to set and return if not defined
+ * @returns {Promise<T>} The value to use
  */
 const monkeyGetSetValue = async (key, value) => {
     const storedValue = await GM.getValue(key);
