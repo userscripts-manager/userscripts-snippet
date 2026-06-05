ff8c368c986623fe7a159a9f26c2949cebf1bc9b
gissehel <public-dev-git-capella@gissehel.org>
2026-06-05 21:37:48 +0200

    Allow to register on a specific node

--- base/registerDomNodeMutated.js
+++ base/registerDomNodeMutated.js
@@ -3,10 +3,15 @@
  * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
  * Use the register pattern thus return the unregister function as a result
  * @param {()=>Promise<void>} callback 
+ * @param {Object} options
+ * @param {HTMLElement} options.rootElement The root element to observe, default to document.documentElement
  * @return {()=>Promise<void>} The unregister function
  */
-const registerDomNodeMutated = async (callback) => {
+const registerDomNodeMutated = async (callback, options) => {
     let callbackInProgress = false
+
+    options = options || {}
+    const rootElement = options.rootElement || document.documentElement;
 
     const action = async () => {
         if (!callbackInProgress) {
@@ -18,7 +23,7 @@ const registerDomNodeMutated = async (callback) => {
 
     const mutationObserver = new MutationObserver(async (mutationsList, observer) => { await action() });
     await action()
-    mutationObserver.observe(document.documentElement, { childList: true, subtree: true });
+    mutationObserver.observe(rootElement, { childList: true, subtree: true });
 
     return async () => mutationObserver.disconnect()
 }
