78cefcd3cdfa1fc26a2616d2c161d58274f27880
gissehel <public-dev-git-capella@gissehel.org>
2026-06-22 11:45:15 +0200

    FIX : Fix typo in copyTextToClipboard

--- base/copyTextToClipboard.js
+++ base/copyTextToClipboard.js
@@ -2,11 +2,11 @@
  * Copy some text to clipboard
  * 
  * @param {string} text text to copy to clipboard
- * @returns 
+ * @returns {Promise<void>}
  */
 const copyTextToClipboard = async (text) => {
     if (!navigator.clipboard) {
-        console.log(`Can't copy [${test}] : No navigator.clipboard API`)
+        console.log(`Can't copy [${text}] : No navigator.clipboard API`)
         return;
     }
 
